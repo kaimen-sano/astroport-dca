0a528e07d9989d24752e9aaa44f211588a57c39a
kaimen-sano <kaimen_sano@protonmail.com>
2022-03-30 20:41:10 +1300

    Add AddBotTip unit tests
    Tests cases where
    * user sends uusd tip
    * user sends no tip
    * user sends non-uusd tip

--- contracts/dca/src/handlers/add_bot_tip.rs
+++ contracts/dca/src/handlers/add_bot_tip.rs
@@ -40,3 +40,75 @@ pub fn add_bot_tip(deps: DepsMut, info: MessageInfo) -> Result<Response, Contrac
         attr("tip_amount", amount),
     ]))
 }
+
+#[cfg(test)]
+mod tests {
+    use astroport::dca::ExecuteMsg;
+    use cosmwasm_std::{
+        attr, coin,
+        testing::{mock_dependencies, mock_env, mock_info},
+        Addr, Response,
+    };
+
+    use crate::{
+        contract::execute,
+        error::ContractError,
+        state::{UserConfig, USER_CONFIG},
+    };
+
+    #[test]
+    fn does_add_bot_tip() {
+        let mut deps = mock_dependencies(&[]);
+
+        let tip_sent = coin(10000, "uusd");
+
+        let info = mock_info("creator", &[tip_sent.clone()]);
+        let msg = ExecuteMsg::AddBotTip {};
+
+        // check that we got the expected response
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap();
+        assert_eq!(
+            res,
+            Response::new().add_attributes(vec![
+                attr("action", "add_bot_tip"),
+                attr("tip_amount", tip_sent.amount)
+            ])
+        );
+
+        // check that user tip balance was added
+        let config = USER_CONFIG
+            .load(&deps.storage, &Addr::unchecked("creator"))
+            .unwrap();
+        assert_eq!(
+            config,
+            UserConfig {
+                tip_balance: tip_sent.amount,
+                ..UserConfig::default()
+            }
+        )
+    }
+
+    #[test]
+    fn does_require_funds() {
+        let mut deps = mock_dependencies(&[]);
+
+        let info = mock_info("creator", &[]);
+        let msg = ExecuteMsg::AddBotTip {};
+
+        // should error with InvalidZeroAmount failure
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap_err();
+        assert_eq!(res, ContractError::InvalidZeroAmount {});
+    }
+
+    #[test]
+    fn does_require_uusd_funds() {
+        let mut deps = mock_dependencies(&[]);
+
+        let info = mock_info("creator", &[coin(20000, "ukrw")]);
+        let msg = ExecuteMsg::AddBotTip {};
+
+        // should error with InvalidZeroAmount
+        let res = execute(deps.as_mut(), mock_env(), info, msg).unwrap_err();
+        assert_eq!(res, ContractError::InvalidZeroAmount {});
+    }
+}
